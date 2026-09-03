bb43d6c27ba17489a6d0afb2114cc41b84d25f1f
Abishekbabu <abishekmaha1905@gmail.com>
2026-09-03 10:32:16 +0530

    Mo1-T01-ST01-L:being the java track

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,11 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
+        System.out.println("Welcome to Java track");
+        System.out.println("i am abishek babu");
+        System.out.println("My gole is to become the java developer");
+        System.out.println("i already know how to build Programing logic");
+        System.out.println("Now i am learning how to express logic in java");
+
        //Write your code here
     }
 }
