dfeb7d70eb68089c247de03c5b69c2b37dec3848
Abishekbabu <abishekmaha1905@gmail.com>
2026-09-05 00:15:13 +0530

    practice hw

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Pratic.java
@@ -0,0 +1,8 @@
+public class Pratic {
+    public static void main(String[] args) {
+       // Display the repository cloning
+       System.out.println("Repository: Cloned Successfully");
+       // Display whether the project is ready.
+       System.out.println("Project Status: Ready for Development");
+    }
+}
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/pratic1.java
@@ -0,0 +1,10 @@
+public class pratic1 {
+    public static void main(String[] args) {
+        System.out.println("--------------------");
+        System.out.println("Name: Alex");
+        System.out.println("Cource: Java");
+        System.out.println("Level: Beginer");
+        System.out.println("--------------------");
+
+    }
+}
